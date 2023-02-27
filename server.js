b27a413ab89547e9c57313dd7cde27ec8403fbaa
blac <badejo.semilore.7@gmail.com>
2023-02-27 11:47:29 +0100

    emit startgame event

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ server.listen(PORT, "127.0.0.1", () => {
   console.log(`listening on port ${PORT}`);
 });
 
+let playerReadyCounter = 0;
+
 io.on("connection", (socket) => {
+  //we only have access to socket when a connection event is handled
   console.log("a user connected", socket.id);
+
+  socket.on("ready", () => {
+    console.log("player ready", socket.id);
+
+    playerReadyCounter++;
+
+    if (playerReadyCounter == 2) {
+      io.emit("startGame", socket.id);
+    }
+  });
 });
