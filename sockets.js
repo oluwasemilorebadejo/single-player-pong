40eca324ec8d3b557bd777048c79e7b9b5a6cb13
blac <badejo.semilore.7@gmail.com>
2023-02-28 15:54:35 +0100

    implemented rooms

--- sockets.js
+++ sockets.js
@@ -2,31 +2,36 @@ let playerReadyCounter = 0;
 
 function listen(io) {
   const pongNameSpace = io.of("/pong");
+  let room;
   pongNameSpace.on("connection", (socket) => {
     //we only have access to socket when a connection event is handled
     console.log("a user connected", socket.id);
 
     socket.on("ready", () => {
-      console.log("player ready", socket.id);
+      room = "room" + Math.floor(playerReadyCounter / 2);
+      socket.join(room);
+
+      console.log("player ready", socket.id, room);
 
       playerReadyCounter++;
 
       if (playerReadyCounter % 2 === 0) {
-        pongNameSpace.emit("startGame", socket.id);
+        pongNameSpace.in(room).emit("startGame", socket.id);
       }
     });
 
     socket.on("paddleMove", (paddleData) => {
       // paddledata is the data received from the paddleMove event that was emitted from the client
-      socket.broadcast.emit("paddleMove", paddleData);
+      socket.to(room).emit("paddleMove", paddleData);
     });
 
     socket.on("ballMove", (ballData) => {
-      socket.broadcast.emit("ballMove", ballData);
+      socket.to(room).emit("ballMove", ballData);
     });
 
     socket.on("disconnect", (reason) => {
       console.log(`Client ${socket.id} disconnected: ${reason}`);
+      socket.leave(room);
     });
   });
 }
